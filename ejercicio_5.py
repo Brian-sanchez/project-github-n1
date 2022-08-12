781f96a4512bb176532e1db60e9d488b07777120
Restyled.io <commits@restyled.io>
2022-08-12 21:56:37 +0000

    Restyled by autopep8

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,14 +1,15 @@
 historial = (2350, 5960, 23000, 1000, 8900)
 
+
 def totales(hist):
     total = 0
     for monto in hist:
         total += monto
-        
+
     if total < 30000:
         print('Gastos por atención de Puppy', total)
-    else:   
+    else:
         print('Gastos por encima de lo presupuestado')
 
-        
+
 totales(historial)

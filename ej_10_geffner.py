096aa0c6adf6593d2d4076b2d49618c29cac451c
Restyled.io <commits@restyled.io>
2022-08-14 05:20:48 +0000

    Restyled by autopep8

--- ej_10_geffner.py
+++ ej_10_geffner.py
@@ -3,8 +3,8 @@
 # Resolucion de Ejercicio Python Nº:
 # 10) Crear una lista denominada “Clientes” con los nombres de los diferentes  dueños de perros.
 # Juan,  Mario,  Ariel,  Josefina,  Marianella.
-clientes = ["Juan",  "Mario",  "Ariel", "Josefina",  "Marianella" ]
+clientes = ["Juan",  "Mario",  "Ariel", "Josefina",  "Marianella"]
 # Ordenarla alfabéticamente y mostrarla por pantalla.
 clientes.sort()
 for clientes in clientes:
-	print(clientes)
+    print(clientes)

46dd6e07a6d658cd6d2ddd578109b088b2a95399
Restyled.io <commits@restyled.io>
2022-08-14 00:56:07 +0000

    Restyled by black

--- val-ian.py
+++ val-ian.py
@@ -3,7 +3,7 @@
 # recorrerla con un bucle  mostrando sus elementos menos el DNI y apellido
 # 23546987(DNI),  “Maria”,  “Perez”(APELLIDO), 4789689,  “Pueyrredon  811”
 
-Dueno2 = [23546987, 'Maria', 'Perez', 4789689, 'Pueyrredon 811']
+Dueno2 = [23546987, "Maria", "Perez", 4789689, "Pueyrredon 811"]
 
 for elemento in Dueno2:
     continue
